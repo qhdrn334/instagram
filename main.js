59408d4811df7bffcd3db3670a0b1a56ec774b1d
qhdrn334 <138760940+qhdrn334@users.noreply.github.com>
2023-11-14 21:39:07 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function()   { 
+   
+   
+   
+$('.insta_heart_icon_outline').click(function(){
+   $('.insta_heart_icon_outline').hide();
+   $('.insta_heart_icon').show();                  // 빈 하트 숨김;
+                                           // 빨간 하트 보이기;
+});
+$('.insta_heart_icon').click(function(){
+   $('.insta_heart_icon_outline').show();
+   $('.insta_heart_icon').hide();                         // 빈 하트 보이기;
+                                             // 빨간 하트 숨기기;
+});
+
+  
+
+
+
+
+
+
+
+
+
+});
+
